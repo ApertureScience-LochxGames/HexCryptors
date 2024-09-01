54ad0f3570d2aaf1b03d713a194631d94bf56c65
Lochx Games/Aperture Science <buggalooskidoo@gmail.com>
2024-08-31 19:28:29 -0600

    Delete HexDecrypter.py

--- HexDecrypter.py
+++
@@ -1,9 +0,0 @@
-from Crypto.Cipher import AES
-import binascii
-
-ciphertext = input("Input text to Unhex: ")
-plaintext = binascii.unhexlify(ciphertext).decode()
-
-print(f'Unhexed Text: {plaintext}')
-
-x = input("Press enter to close...")
