efb39137ab12f1294e136c7ee81968608bf9a286
Lochx Games/Aperture Science <buggalooskidoo@gmail.com>
2024-08-31 19:28:08 -0600

    Delete HexEncrypter.py

--- HexEncrypter.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import AES
-
-plaintext = input("Input text to Hex: ")
-ciphertext = plaintext.encode()
-
-print(f'Hexed Text: {ciphertext.hex()}')
-
-x = input("Press enter to close...")
